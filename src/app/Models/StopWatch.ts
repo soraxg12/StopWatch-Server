63664410a2c738343e7ab571b81afee7d9cdf6b8
soraxg12 <contadevervidio@gmail.com>
2021-06-14 22:18:40 -0300

    add Model stopwatch

---
+++ src/app/Models/StopWatch.ts
@@ -0,0 +1,21 @@
+import { Document, Model } from 'mongoose';
+import mongoose from '../../Database';
+
+//interface do Schema do banco de dados
+export interface IStopWatch extends Document {
+    time:number;
+    minutes:number;
+    seconds:number;
+    createdAt: Date;
+};
+//Schema do banco de dados
+const StopWatchSchema = new mongoose.Schema({
+    time: { type: Number, required: true },
+    minutes: { type: Number, required: true },
+    seconds: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const StopWatch: Model<IStopWatch> = mongoose.model('StopWatch', StopWatchSchema);
+
+export { StopWatch  };
