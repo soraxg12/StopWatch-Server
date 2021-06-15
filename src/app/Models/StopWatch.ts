7b092ff2e2a36b53be3e7f1a073032f59091880f
soraxg12 <contadevervidio@gmail.com>
2021-06-14 22:25:15 -0300

    updating stopwatch model

--- src/app/Models/StopWatch.ts
+++ src/app/Models/StopWatch.ts
@@ -3,9 +3,9 @@ import mongoose from '../../Database';
 
 //interface do Schema do banco de dados
 export interface IStopWatch extends Document {
-    time:number;
-    minutes:number;
-    seconds:number;
+    time: number;
+    minutes: number;
+    seconds: number;
     createdAt: Date;
 };
 //Schema do banco de dados
@@ -18,4 +18,4 @@ const StopWatchSchema = new mongoose.Schema({
 
 const StopWatch: Model<IStopWatch> = mongoose.model('StopWatch', StopWatchSchema);
 
-export { StopWatch  };
+export { StopWatch };
